958690167dd396a87fe0b3103997e72dd37a4dd3
Isa SUMER <sumerisa5@gmail.com>
2021-09-07 02:51:13 +0300

    intro designed

--- website/src/components/Intro.jsx
+++ website/src/components/Intro.jsx
@@ -1,16 +1,109 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Woman from "./img/woman2.png";
 
 const Container = styled.div`
-height:calc(100vh - 50px);
-background-color: red;
+  height: calc(100vh - 50px);
+  display: flex;
+`;
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  width: 60%;
+`;
+
+const Desc = styled.p`
+  width: 60%;
+  font-size: 20px;
+  margin-top: 20px;
+`;
+
+const Info = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 50px;
+  align-items: center;
+`;
+
+const Right = styled.div`
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  background-color: blue;
+  padding: 15px;
+  color: white;
+  border-radius: 10px;
+  font-weight: bold;
+  border: none;
+  letter-spacing: 2px;
+  cursor: pointer;
+`;
+const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Phone = styled.span`
+  color: pink;
+  font-weight: bold;
+`;
+const ContactText = styled.span`
+  color: gray;
+  margin-top: 5px;
+`;
+
+const Image = styled.img`
+  height: 60%;
+`;
+
+const IntroShape=styled.div `
+width: 100%;
+height:100;
+position:absolute;
+top:0px;
+left:0px;
+clip-path: polygon(80% 0, 100% 0%, 100% 100%, 48% 100%, 64% 43%);
+background-color: crimson;
+z-index:-1;
 `
 const Intro = () => {
-    return (
-        <Container>
-            
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Left>
+        <Title>Lorem ipsum dolor sit amet</Title>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis
+          temporibus quos officia cupiditate voluptates iusto id laudantium
+          reiciendis quisquam.
+        </Desc>
+        <Info>
+          <Button>This is Button</Button>
+          <Contact>
+            <Phone>Call Us (012) 345 - 6789</Phone>
+            <ContactText>
+              Lorem ipsum dolor sit amet consectetur adipisicing
+            </ContactText>
+          </Contact>
+        </Info>
+      </Left>
+      <Right>
+        <Image src={Woman}></Image>
+      </Right>
+    </Container>
+  );
+};
 
-export default Intro
+export default Intro;
