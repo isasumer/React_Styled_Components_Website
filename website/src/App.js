b0975f3cd677e96180cc67115db6ce1767bd7454
Isa SUMER <sumerisa5@gmail.com>
2021-09-07 02:51:01 +0300

    woman photo and intoshape added

--- website/src/App.js
+++ website/src/App.js
@@ -5,12 +5,24 @@ import Intro from "./components/Intro";
 
 const Container =styled.div`
 height:100vh;
+overflow:hidden;
+`
+const IntoShape=styled.div `
+width: 100%;
+height:100%;
+position:absolute;
+top:0px;
+left:0px;
+clip-path: polygon(80% 0, 100% 0%, 100% 100%, 48% 100%, 64% 43%);
+background-color: crimson;
+z-index:-1;
 `
 const App = () => {
   return (
     <Container>
       <Navbar />
       <Intro />
+      <IntoShape/>
     </Container>
   );
 };
