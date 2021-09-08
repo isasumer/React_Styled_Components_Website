8692ab13b26bca4cdd4e0edae2865db4a8cc506c
Isa SUMER <sumerisa5@gmail.com>
2021-09-08 13:10:21 +0300

    fetaure responsive completed

--- website/src/components/Feature.js
+++ website/src/components/Feature.js
@@ -5,48 +5,69 @@ import AnimatedShapes from "./AnimatedShapes";
 
 const Container = styled.div`
   display: flex;
-  margin-top:30px;
+  margin-top: 30px;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+    padding: 30px 20px;
+  }
 `;
 const Left = styled.div`
-
   display: flex;
-
   width: 50%;
+  @media only screen and (max-width: 480px) {
+    display: none;
 `;
 const Image = styled.img`
   /* width: 50%; */
   /* height: 50%;*/
+
+  }
 `;
 const Right = styled.div`
   display: flex;
-  flex-direction:column;
+  flex-direction: column;
   justify-content: center;
   width: 50%;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
 `;
 
 const Title = styled.span`
-font-size: 70px;
+  font-size: 50px;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
 `;
 const SubTitle = styled.span`
-font-size: 24px;
-font-style:italic;
-color:#494949;
-margin-top:30px;
+  font-size: 24px;
+  font-style: italic;
+  color: #494949;
+  margin-top: 30px;
+  @media only screen and (max-width: 480px) {
+    font-size: 22px;
+    width: 100%;
+  }
 `;
 const Desc = styled.p`
-font-size: 20px;
-color:#777;
-margin-top:20px;`
+  font-size: 20px;
+  color: #777;
+  margin-top: 20px;
+  @media only screen and (max-width: 480px) {
+    font-size: 18px;
+    width: 100%;
+  }
+`;
 
 const Button = styled.button`
-width:150px;
-border:none;
-padding:15px 20px;
-background-color:blue;
-color:white;
-border-radius:10px;
-margin-top:30px;
-cursor: pointer;
+  width: 150px;
+  border: none;
+  padding: 15px 20px;
+  background-color: blue;
+  color: white;
+  border-radius: 10px;
+  margin-top: 30px;
+  cursor: pointer;
 `;
 
 const Feature = () => {
@@ -57,8 +78,8 @@ const Feature = () => {
       </Left>
       <Right>
         <Title>
-          <b>Lorem</b>  ipsum <br></br>
-          <b>Lorem  </b> dolor<br></br>
+          <b>Lorem</b> ipsum <br></br>
+          <b>Lorem </b> dolor<br></br>
         </Title>
         <SubTitle>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
@@ -70,7 +91,7 @@ const Feature = () => {
         </Desc>
         <Button>Learn More</Button>
       </Right>
-      <AnimatedShapes/>
+      <AnimatedShapes />
     </Container>
   );
 };
