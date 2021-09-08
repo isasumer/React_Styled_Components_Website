a254d57d8ccffe2946b2054220940c9717134ff3
Isa SUMER <sumerisa5@gmail.com>
2021-09-08 12:26:50 +0300

    footer completed

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 10%;
+  background-color: black;
+  color: lightgray;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top:30px;
+  
+`;
+const List = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  
+`;
+const ListItem = styled.li`
+  margin-left: 20px;
+`;
+const Copyright = styled.span`
+margin-right: 40px;
+`;
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Wrapper>
+        <List>
+          <ListItem>Guide</ListItem>
+          <ListItem>Support</ListItem>
+          <ListItem>API</ListItem>
+          <ListItem>Community</ListItem>
+        </List>
+        <Copyright>Copyright (c)</Copyright>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;
